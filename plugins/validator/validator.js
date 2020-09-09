29caa93e905d26e933acde41117afe0e28bb1653
Limon1980 <tarkalmob@yandex.ru>
2020-09-09 21:07:02 +1000

    валидатор по видео сделал, ничего не понял.

--- plugins/validator/validator.js
+++ plugins/validator/validator.js
@@ -8,22 +8,69 @@ class Validator{
     this.elementsForm = [...this.form.elements].filter( item => {
       return item.tagName.toLowerCase() !== 'button' && item.type !== 'button';
      });
+    this.error = new Set();
   }
 
   init(){
     this.applyStele();
-    this.elementsForm.forEach(elem => elem.addEventListener('change', this.checkIt));
-    
+    this.setPattern();
+    this.elementsForm.forEach(elem => elem.addEventListener('change', this.checkIt.bind(this)));
+    this.form.addEventListener('submit', e => {
+      this.elementsForm.forEach(elem => this.checkIt({target: elem}));
+      if (this.error.size){
+        e.preventDefault();
+      }
+    })
   }
 
-  checkIt(){
-    console.log(event);
+  isValid(elem){
+    const validatorMethod = {
+      notEmpty(elem) {
+        if (elem.value.trim() ===  ''){
+          return false;
+        }
+        return true;
+      },
+      pattern(elem, pattern){
+        return pattern.test(elem.value);
+      }
+    };
+    if (this.method){
+      const method = this.method[elem.id];
+      if (method){
+        return method.every( item => {
+          
+          return  validatorMethod[item[0]](elem, this.pattern[item[1]]);
+        });
+      } else {
+        console.warn('Необходимо передать id полей ввода и методы');
+      }
+    }
+   
+    return true;
+  }
+
+  checkIt(event){
+    const target = event.target;
+
+    if(this.isValid(target)){
+      this.showSuccess(target);
+      this.error.delete(target);
+    } else {
+      this.showError(target);
+      this.error.add(target);
+    }
+
   }
 
   showError(elem){
     elem.classList.remove('success'); 
     elem.classList.add('error');
+    if (elem.nextElementSibling && elem.nextElementSibling.classList.contains('validator-error')){
+      return;
+    }
     const erroeDiv = document.createElement('div');
+    // console.log(erroeDiv);
     erroeDiv.textContent = 'Ошибка в этом поле';
     erroeDiv.classList.add('validator-error');
     elem.insertAdjacentElement('afterend', erroeDiv);
@@ -32,7 +79,8 @@ class Validator{
   showSuccess(elem){
     elem.classList.remove('error');
   elem.classList.add('success'); 
-    if (elem.nextElementSibling.classList.contais('validator-error')){
+  // console.log(elem.nextElementSibling.classList);
+    if (elem.nextElementSibling && elem.nextElementSibling.classList.contains('validator-error')){
       elem.nextElementSibling.remove();
     }  
   }
@@ -43,14 +91,28 @@ class Validator{
     input.success {
       border: 2px solid green
     }
-    iput.error {
+    input.error {
       border: 2px solid red
     }
-    validator-error {
-      font-size: 14px;
+    .validator-error {
+      font-size: 12px;
+      font-family: sans-serif;
       color: red
     }
     `;
     document.head.appendChild(style);
+  }
+
+  setPattern(){
+
+    if (!this.pattern.phone){
+      this.pattern.phone = /^\+?[78]([-()]*\d){10}$/;
+    }
+
+    if (!this.pattern.email){
+      this.pattern.email = /^\w+@\w+\.\w{2,}$/;
+    }
+
+  
   }
 }
